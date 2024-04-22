5f3ffb4ec3614f1b75429b67c62cf26103c9efbe
bilge <bilgehankilic.official@gmail.com>
2024-04-22 23:29:16 +0300

    class 22 is finish

--- js_ders22/scripts.js
+++ js_ders22/scripts.js
@@ -1,6 +1,8 @@
 const textInputDOM = document.getElementById("todo_input");
 const btnAddTodoDOM = document.getElementById("add_todo");
 const todosDOM = document.querySelector("#todos");
+const btnClearDOM = document.querySelector("#clear");
+
 
 
 let textInputValue = "";
@@ -12,12 +14,13 @@ textInputDOM.addEventListener("change", function (event) {
 });
 
 btnAddTodoDOM.addEventListener("click", addTodo);
+btnClearDOM.addEventListener("click", clearTodos);
 
 function addTodo(e) {
     e.preventDefault();
     let todosLength = todos.length;
     todos.unshift({
-        
+
         id: todosLength + 1,
         todoTitle: textInputValue
     });
@@ -29,18 +32,40 @@ function addTodo(e) {
 
 function displayTodos() {
     let result = "";
+    if (todos.length === 0) {
+        todosDOM.innerHTML = "liste boş !"
+    } else {
+        todos.forEach((item) => {
+            result += ` 
+            <li class="flex justify-between border px-4 py-3
+           flex items-center justify-between">
+                <span>${item.todoTitle}</span>
+                <button class="text-red-400" onclick="deleteTodo( ${item.id})">Sil</button>
+            </li>
+            ${item.id}
+            `;
+        });
+        todosDOM.innerHTML = result;
 
-    todos.forEach((item) => {
-        result += ` 
-        <li class="flex justify-between border px-4 py-3
-       flex items-center justify-between">
-            <span>${item.todoTitle}</span>
-            <button class="text-red-400">Sil</button>
-        </li>
-        ${item.id};
-        `;
-    });
-    todosDOM.innerHTML = result;
-    displayTodos();
+    };
 
 };
+
+function deleteTodo(id) {
+    let deletedId;
+
+    for (let index in todos) {
+        if (todos[index].id == id) {
+            deletedId = index;
+        };
+    };
+    todos.splice(deletedId, 1);
+    displayTodos();
+}
+
+function clearTodos() {
+    todos.splice(0, todos.length);
+    displayTodos();
+}
+
+displayTodos();
